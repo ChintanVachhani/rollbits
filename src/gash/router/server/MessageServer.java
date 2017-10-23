016ae651b13c3bfa61474b285e8759b21c3330ea
Chintan Vachhani <chintan.vachhani10@gmail.com>
2017-10-23 15:30:21 -0700

    Basic network discovery setup.

--- src/gash/router/server/MessageServer.java
+++ src/gash/router/server/MessageServer.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Gash.
- *
+ * <p>
  * This file and intellectual content is protected under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -34,172 +34,184 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class MessageServer {
-	protected static Logger logger = LoggerFactory.getLogger("server");
+    protected static Logger logger = LoggerFactory.getLogger("server");
 
-	protected static HashMap<Integer, ServerBootstrap> bootstrap = new HashMap<Integer, ServerBootstrap>();
+    protected static HashMap<Integer, ServerBootstrap> bootstrap = new HashMap<Integer, ServerBootstrap>();
 
-	public static final String sPort = "port";
-	public static final String sPoolSize = "pool.size";
+    public static final String sPort = "port";
+    public static final String sPoolSize = "pool.size";
 
-	protected RoutingConf conf;
-	protected boolean background = false;
+    protected RoutingConf conf;
+    protected boolean background = false;
 
-	public MessageServer(RoutingConf conf) {
-		this.conf = conf;
-	}
+    public MessageServer(RoutingConf conf) {
+        this.conf = conf;
+    }
 
-	public void release() {
-	}
+    public void release() {
+    }
 
-	public void startServer() {
-		StartCommunication comm = new StartCommunication(conf);
-		logger.info("Communication starting");
+    public void startServer() {
+        // network discovery mechanism using UDP
+        // listening for discovery request by other servers
+        Thread discoveryThread = new Thread(DiscoveryThread.getInstance());
+        discoveryThread.start();
+        // finding all the active servers
+        DiscoveryClient dc = new DiscoveryClient();
+        dc.client();
 
-		if (background) {
-			Thread cthread = new Thread(comm);
-			cthread.start();
-		} else
-			comm.run();
-	}
+        // displaying server map
+        logger.info(String.valueOf(ServerMap.servers));
 
-	/**
-	 * static because we need to get a handle to the factory from the shutdown
-	 * resource
-	 */
-	public static void shutdown() {
-		logger.info("Server shutdown");
-		System.exit(0);
-	}
+        // start communication over channel
+        StartCommunication comm = new StartCommunication(conf);
+        logger.info("Communication starting");
 
-	/**
-	 * initialize the server with a configuration of it's resources
-	 * 
-	 * @param cfg
-	 */
-	public MessageServer(File cfg) {
-		init(cfg);
-	}
+        if (background) {
+            Thread cthread = new Thread(comm);
+            cthread.start();
+        } else
+            comm.run();
+    }
 
-	private void init(File cfg) {
-		if (!cfg.exists())
-			throw new RuntimeException(cfg.getAbsolutePath() + " not found");
-		// resource initialization - how message are processed
-		BufferedInputStream br = null;
-		try {
-			byte[] raw = new byte[(int) cfg.length()];
-			br = new BufferedInputStream(new FileInputStream(cfg));
-			br.read(raw);
-			conf = JsonUtil.decode(new String(raw), RoutingConf.class);
-			if (!verifyConf(conf))
-				throw new RuntimeException("verification of configuration failed");
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    /**
+     * static because we need to get a handle to the factory from the shutdown
+     * resource
+     */
+    public static void shutdown() {
+        logger.info("Server shutdown");
+        System.exit(0);
+    }
 
-	private boolean verifyConf(RoutingConf conf) {
-		return (conf != null);
-	}
+    /**
+     * initialize the server with a configuration of it's resources
+     *
+     * @param cfg
+     */
+    public MessageServer(File cfg) {
+        init(cfg);
+    }
 
-	/**
-	 * initialize netty communication
-	 * 
-	 * @param port
-	 *            The port to listen to
-	 */
-	private static class StartCommunication implements Runnable {
-		RoutingConf conf;
+    private void init(File cfg) {
+        if (!cfg.exists())
+            throw new RuntimeException(cfg.getAbsolutePath() + " not found");
+        // resource initialization - how message are processed
+        BufferedInputStream br = null;
+        try {
+            byte[] raw = new byte[(int) cfg.length()];
+            br = new BufferedInputStream(new FileInputStream(cfg));
+            br.read(raw);
+            conf = JsonUtil.decode(new String(raw), RoutingConf.class);
+            if (!verifyConf(conf))
+                throw new RuntimeException("verification of configuration failed");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
-		public StartCommunication(RoutingConf conf) {
-			this.conf = conf;
-		}
+    private boolean verifyConf(RoutingConf conf) {
+        return (conf != null);
+    }
 
-		public void run() {
-			// construct boss and worker threads (num threads = number of cores)
+    /**
+     * initialize netty communication
+     *
+     * @param port
+     *            The port to listen to
+     */
+    private static class StartCommunication implements Runnable {
+        RoutingConf conf;
 
-			EventLoopGroup bossGroup = new NioEventLoopGroup();
-			EventLoopGroup workerGroup = new NioEventLoopGroup();
+        public StartCommunication(RoutingConf conf) {
+            this.conf = conf;
+        }
 
-			try {
-				ServerBootstrap b = new ServerBootstrap();
-				bootstrap.put(conf.getPort(), b);
+        public void run() {
+            // construct boss and worker threads (num threads = number of cores)
 
-				b.group(bossGroup, workerGroup);
-				b.channel(NioServerSocketChannel.class);
-				b.option(ChannelOption.SO_BACKLOG, 100);
-				b.option(ChannelOption.TCP_NODELAY, true);
-				b.option(ChannelOption.SO_KEEPALIVE, true);
-				// b.option(ChannelOption.MESSAGE_SIZE_ESTIMATOR);
+            EventLoopGroup bossGroup = new NioEventLoopGroup();
+            EventLoopGroup workerGroup = new NioEventLoopGroup();
 
-				boolean compressComm = false;
-				b.childHandler(new ServerInit(conf, compressComm));
+            try {
+                ServerBootstrap b = new ServerBootstrap();
+                bootstrap.put(conf.getPort(), b);
 
-				// Start the server.
-				logger.info("Starting server, listening on port = " + conf.getPort());
-				ChannelFuture f = b.bind(conf.getPort()).syncUninterruptibly();
+                b.group(bossGroup, workerGroup);
+                b.channel(NioServerSocketChannel.class);
+                b.option(ChannelOption.SO_BACKLOG, 100);
+                b.option(ChannelOption.TCP_NODELAY, true);
+                b.option(ChannelOption.SO_KEEPALIVE, true);
+                // b.option(ChannelOption.MESSAGE_SIZE_ESTIMATOR);
 
-				logger.info(f.channel().localAddress() + " -> open: " + f.channel().isOpen() + ", write: "
-						+ f.channel().isWritable() + ", act: " + f.channel().isActive());
+                boolean compressComm = false;
+                b.childHandler(new ServerInit(conf, compressComm));
 
-				// block until the server socket is closed.
-				f.channel().closeFuture().sync();
+                // Start the server.
+                logger.info("Starting server, listening on port = " + conf.getPort());
+                ChannelFuture f = b.bind(conf.getPort()).syncUninterruptibly();
 
-			} catch (Exception ex) {
-				// on bind().sync()
-				logger.error("Failed to setup handler.", ex);
-			} finally {
-				// Shut down all event loops to terminate all threads.
-				bossGroup.shutdownGracefully();
-				workerGroup.shutdownGracefully();
-			}
-		}
-	}
+                logger.info(f.channel().localAddress() + " -> open: " + f.channel().isOpen() + ", write: "
+                        + f.channel().isWritable() + ", act: " + f.channel().isActive());
 
-	/**
-	 * help with processing the configuration information
-	 * 
-	 * @author gash
-	 *
-	 */
-	public static class JsonUtil {
-		private static JsonUtil instance;
+                // block until the server socket is closed.
+                f.channel().closeFuture().sync();
 
-		public static void init(File cfg) {
+            } catch (Exception ex) {
+                // on bind().sync()
+                logger.error("Failed to setup handler.", ex);
+            } finally {
+                // Shut down all event loops to terminate all threads.
+                bossGroup.shutdownGracefully();
+                workerGroup.shutdownGracefully();
+            }
+        }
+    }
 
-		}
+    /**
+     * help with processing the configuration information
+     *
+     * @author gash
+     *
+     */
+    public static class JsonUtil {
+        private static JsonUtil instance;
 
-		public static JsonUtil getInstance() {
-			if (instance == null)
-				throw new RuntimeException("Server has not been initialized");
+        public static void init(File cfg) {
 
-			return instance;
-		}
+        }
 
-		public static String encode(Object data) {
-			try {
-				ObjectMapper mapper = new ObjectMapper();
-				return mapper.writeValueAsString(data);
-			} catch (Exception ex) {
-				return null;
-			}
-		}
+        public static JsonUtil getInstance() {
+            if (instance == null)
+                throw new RuntimeException("Server has not been initialized");
 
-		public static <T> T decode(String data, Class<T> theClass) {
-			try {
-				ObjectMapper mapper = new ObjectMapper();
-				return mapper.readValue(data.getBytes(), theClass);
-			} catch (Exception ex) {
-				return null;
-			}
-		}
-	}
+            return instance;
+        }
+
+        public static String encode(Object data) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                return mapper.writeValueAsString(data);
+            } catch (Exception ex) {
+                return null;
+            }
+        }
+
+        public static <T> T decode(String data, Class<T> theClass) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                return mapper.readValue(data.getBytes(), theClass);
+            } catch (Exception ex) {
+                return null;
+            }
+        }
+    }
 
 }
---
+++ src/gash/router/server/ServerMap.java
@@ -0,0 +1,8 @@
+package gash.router.server;
+
+import java.net.InetAddress;
+import java.util.HashSet;
+
+public class ServerMap {
+    public static HashSet<InetAddress> servers = new HashSet<InetAddress>();
+}
