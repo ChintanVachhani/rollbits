f80b9f9e1a878349d1f23d899c3a269ab7fbe698
arvind <doorknob88@gmail.com>
2017-11-13 20:07:17 -0800

    updated and changed to singleton

--- src/gash/router/server/RoutingMap.java
+++ src/gash/router/server/RoutingMap.java
@@ -1,10 +1,45 @@
 package gash.router.server;
 
-import java.net.InetAddress;
 import java.util.HashSet;
 
 public class RoutingMap {
-    public static HashSet<String> internalServers = new HashSet<String>();
-    public static HashSet<String> externalServers = new HashSet<>();
-    public static HashSet<String> clients = new HashSet<>();
+    private static RoutingMap ourInstance = new RoutingMap();
+
+    public static RoutingMap getInstance() {
+        return ourInstance;
+    }
+
+    private static HashSet<String> internalServers;
+    private static HashSet<String> externalServers;
+    private static HashSet<String> clients;
+
+    private RoutingMap() {
+        internalServers = new HashSet<>();
+        externalServers = new HashSet<>();
+        clients = new HashSet<>();
+    }
+
+    public static HashSet<String> getInternalServers() {
+        return internalServers;
+    }
+
+    public static void setInternalServers(HashSet<String> internalServers) {
+        RoutingMap.internalServers = internalServers;
+    }
+
+    public static HashSet<String> getExternalServers() {
+        return externalServers;
+    }
+
+    public static void setExternalServers(HashSet<String> externalServers) {
+        RoutingMap.externalServers = externalServers;
+    }
+
+    public static HashSet<String> getClients() {
+        return clients;
+    }
+
+    public static void setClients(HashSet<String> clients) {
+        RoutingMap.clients = clients;
+    }
 }
