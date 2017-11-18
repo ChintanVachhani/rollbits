cacb76f26bd2a43d61b578c888af0199ad8d4562
arvind <doorknob88@gmail.com>
2017-11-17 16:32:16 -0800

    Updated raft

--- src/gash/router/server/raft/Raft.java
+++ src/gash/router/server/raft/Raft.java
@@ -3,156 +3,94 @@ package gash.router.server.raft;
 import gash.router.container.RoutingConf;
 import gash.router.server.Node;
 import gash.router.server.RoutingMap;
-import gash.router.server.resources.RouteResource;
-import io.netty.channel.Channel;
 import org.slf4j.LoggerFactory;
-import routing.Pipe;
-import sun.rmi.runtime.Log;
 
-import java.beans.Beans;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
-import java.util.Random;
+import java.util.concurrent.*;
 
 import static java.lang.Thread.sleep;
 
 public class Raft {
-    private static Raft ourInstance = new Raft();
     protected static org.slf4j.Logger logger = LoggerFactory.getLogger("Raft");
+
+    private static Raft ourInstance = new Raft();
     public static Raft getInstance() {
         return ourInstance;
     }
-
     private HashMap<String, String> routing;
     private RoutingConf conf;
-
     private String myIP;
-    private Integer currentHeartBeatID = 0;
+    private boolean iAmLeader;
+    private Raft() {
+        myIP = conf.getNodeAddress();
+        iAmLeader = false;
+    }
+
     public void setConf(RoutingConf conf) {
         this.conf = conf;
     }
 
-    private class HeartBeat {
-        String IP;
-        private ArrayList<Pipe.Route> messageBuffer;
-        public HeartBeat(String pramMyIP, ArrayList<Pipe.Route> pramMessageBuffer){
-            IP = pramMyIP;
-            messageBuffer = pramMessageBuffer;
-        }
-    }
-
-    private Raft() {
-        while (conf == null){
-            try {
-                sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        myIP = conf.getNodeAddress();
-    }
-
-    private String getLeaderIP(){
+    private String getLeaderIP() {
         Integer max = Integer.MIN_VALUE;
         String maxIP = "";
-        for (Node node : RoutingMap.getInstance().getInternalServers().values()){
+        for (Node node : RoutingMap.getInstance().getInternalServers().values()) {
             String ip = node.getNodeAddress();
-            Integer lastNumber = Integer.parseInt(ip.substring(ip.lastIndexOf('.'), ip.length()-1));
-            if (max < lastNumber){
+            Integer lastNumber = Integer.parseInt(ip.substring(ip.lastIndexOf('.'), ip.length() - 1));
+            if (max < lastNumber) {
                 maxIP = node.getNodeAddress();
             }
         }
         return maxIP;
     }
 
-    public boolean isLeader(){
-        if(Objects.equals(conf.getNodeAddress(), getLeaderIP())){
+    public boolean isLeader() {
+        if (Objects.equals(conf.getNodeAddress(), getLeaderIP())) {
             return true;
-        }
-        else return false;
+        } else return false;
     }
 
-    public void respondHeartBeat(HeartBeat heartBeat){
-        if (isLeader()){return;}
-
-        if (heartBeat.IP != null){
-            if (RoutingMap.getInstance().getInternalServers().containsKey(heartBeat.IP)){
-                //Heartbeat is from known server list, process it here
-                //TODO: process Heartbeat here.
-
-            }
-            else {
-                //IP exists but is not in internal servers network, add it to map
-                System.out.println("Heartbeat received from address not in internal server list!: "+heartBeat.IP+" adding to internal server list now..");
-                //TODO: RoutingMap.getInstance().getInternalServers().add(heartBeat.IP);
-                //TODO: process heartbeat here
-
-            }
-
-        }
-        else {
-            //inetaddress is null throw error
-            System.out.println("Error! heartbeat.IP is null");
-        }
-    }
-
-    public void sendHeartBeat(){
-        currentHeartBeatID++;
-        for(Node node : RoutingMap.getInstance().getInternalServers().values()){
-            if (!Objects.equals(node.getNodeAddress(), myIP)){
+    public void sendHeartBeat() {
+        for (Node node : RoutingMap.getInstance().getInternalServers().values()) {
+            if (!Objects.equals(node.getNodeAddress(), myIP)) {
                 //TODO: write code to send heartbeat via TCP
 
             }
         }
     }
 
-    public void handleMessage(Pipe.Route msg) {
-        if (msg == null) {
-            // TODO add logging
-            System.out.println("ERROR: Unexpected content - " + msg);
-            return;
-        }
+    public void election(){
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+        final Future handler = executor.submit((Callable) () -> {
+            sleep(1000);
+            for(Node node: RoutingMap.getInstance().getInternalServers().values()){
+                if (compareIP(conf.getNodeAddress(), node.getNodeAddress()) == -1 ){
 
-        System.out.println("---> " + msg.getId() + ": " + msg.getPath());
-
-        try {
-            System.out.println("/" + msg.getPath().toString().toLowerCase());
-            String clazz = routing.get("/" + msg.getPath().toString().toLowerCase());
-            if (clazz != null) {
-                RouteResource rsc = (RouteResource) Beans.instantiate(RouteResource.class.getClassLoader(), clazz);
-            } else {
-                // TODO add logging
-                System.out.println("ERROR: unknown path - " + msg.getPath());
+                }
             }
-        } catch (Exception ex) {
-            // TODO add logging
-            System.out.println("ERROR: processing request - " + ex.getMessage());
+            return null;
+        });
+        executor.schedule(new Runnable(){
+            public void run(){
+                handler.cancel(true);
+            }
+        }, 10000, TimeUnit.MILLISECONDS);
+    }
+
+    public Integer compareIP(String ip1S,String ip2S){
+        Integer ip1 = Integer.parseInt(ip1S.substring(ip1S.lastIndexOf('.'), ip1S.length() -1 ));
+        Integer ip2 = Integer.parseInt(ip2S.substring(ip2S.lastIndexOf('.'), ip2S.length() -1 ));
+        if(ip1 > ip2){
+            return 1;
         }
-
-        System.out.flush();
-    }
-
-
-    public LogReplication getLogReplication() {
-        return LogReplication.getInstance();
-    }
-
-    class TCPServer {
-        public void main(String argv[]) throws Exception {
-
-
-
+        else if (ip1 == ip2){
+            return 0;
         }
+        else return -1;
     }
 
+
+    public boolean isiAmLeader() {
+        return iAmLeader;
+    }
 }
